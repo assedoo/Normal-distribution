932756b8b116ed6a1a325f1c6b9f1759d1e71ec8
Pavel Zastavnyi <assedoo@gmail.com>
2013-10-13 12:00:56 +0300

    Add tools

---
+++ src/main/java/ua/net/assedo/normaldistribution/tools/ArithmeticProgression.java
@@ -0,0 +1,14 @@
+package ua.net.assedo.normaldistribution.tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: assedoo@gmail.com
+ * Project: second
+ * Date: 10/12/13
+ * Time: 7:50 PM
+ */
+public class ArithmeticProgression {
+    public static int sum(int n) {
+        return n*(2+(n-1))/2;
+    }
+}
